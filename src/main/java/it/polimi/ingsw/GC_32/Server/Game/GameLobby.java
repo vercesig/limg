525e453fbe247efb041cbb17540bfc2a439e3b18
Giacomo Vercesi <giacomo.vercesi@mail.polimi.it>
2017-06-07 23:18:02 +0200

    Add graceful player waiting in lobby

--- src/main/java/it/polimi/ingsw/GC_32/Server/Game/GameLobby.java
+++ src/main/java/it/polimi/ingsw/GC_32/Server/Game/GameLobby.java
@@ -1,14 +1,13 @@
 package it.polimi.ingsw.GC_32.Server.Game;
 
 import java.io.IOException;
-
 import it.polimi.ingsw.GC_32.Server.Network.MessageManager;
 import it.polimi.ingsw.GC_32.Server.Network.PlayerRegistry;
 
 public class GameLobby {
 	
 	private int min_players = 2;
-	private int startGameTimeout = 5000;
+	private int startGameTimeout = 50000;
 	private Game game;
 	
 	public GameLobby() throws IOException, InterruptedException{
@@ -17,18 +16,25 @@ public class GameLobby {
 			if(PlayerRegistry.getInstance().getConnectedPlayers().size()>=min_players){
 				System.out.println("[GAMELOBBY] minimum number of players ("+min_players+") achieved.");
 				System.out.println("[GAMELOBBY] timeout started, waiting for other players");
-				Thread.sleep(startGameTimeout);
-				System.out.println("[GAMELOBBY] timeout finished. starting new game...");
-				game = new Game(PlayerRegistry.getInstance().getConnectedPlayers());
-				System.out.println("[GAMELOBBY] new game created with "+game.getPlayerList().size()+" players");
-				MessageManager.getInstance().registerGame(game);
+				long startTimeoutTime = System.currentTimeMillis();
+				while(true){
+					if(startTimeoutTime + startGameTimeout > System.currentTimeMillis()){
+						System.out.println("[GAMELOBBY] timeout finished. starting new game...");
+						break;
+					}
+					if(PlayerRegistry.getInstance().getConnectedPlayers().size()==4){
+						break;
+					}
+					Thread.sleep(200);
+				}
 				break;
 			}
 		}
+		game = new Game(PlayerRegistry.getInstance().getConnectedPlayers());
+		System.out.println("[GAMELOBBY] new game created with "+game.getPlayerList().size()+" players");
+		MessageManager.getInstance().registerGame(game);
 		System.out.println("[GAMELOBBY] launching game thread");
 		Thread gameThread = new Thread(game);
 		gameThread.start();
-		
 	}
-	
 }
