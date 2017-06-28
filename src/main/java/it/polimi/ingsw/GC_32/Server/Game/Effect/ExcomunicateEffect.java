29cb8dc96202d1c583103628e2d317501f052c42
VaporUser <enryruggi@hotmail.it>
2017-06-28 18:33:56 +0200

    Excommunication commit

--- src/main/java/it/polimi/ingsw/GC_32/Server/Game/Effect/ExcomunicateEffect.java
+++ src/main/java/it/polimi/ingsw/GC_32/Server/Game/Effect/ExcomunicateEffect.java
@@ -7,7 +7,9 @@ import it.polimi.ingsw.GC_32.Server.Game.ContextManager;
 import it.polimi.ingsw.GC_32.Server.Game.Player;
 import it.polimi.ingsw.GC_32.Server.Game.Board.Board;
 
-public class ExcomunicateLessResourceEffect {
+public class ExcomunicateEffect {
+	
+	// Excommunication card 1-1, 1-2, 1-3, 1-4
 	static EffectBuilder lessEffectBuilder = (JsonValue json) -> {
 		
 		String excommunicateFlag = json.asObject().get("RESOURCE").asString();
@@ -17,7 +19,19 @@ public class ExcomunicateLessResourceEffect {
 		return e;
 	};
 
+	// Excommunication card 3-1, 3-2, 3-3, 3-4, 3-5, 3-6, 3-7
+	static EffectBuilder noEndPoint = (JsonValue json) -> {
+		
+		String excommunicateFlag = json.asObject().get("FLAG").asString();
+		Effect e = (Board b, Player p, Action a, ContextManager cm) -> {
+			p.getExcomunicateFlag().add(excommunicateFlag);
+		};
+		return e;
+	};
+	
 	public static void loadBuilder(){
 		EffectRegistry.getInstance().registerBuilder("LESSRESOURCE", lessEffectBuilder);
+		EffectRegistry.getInstance().registerBuilder("NOENDPOINTS", noEndPoint);
+
 	}
 }
