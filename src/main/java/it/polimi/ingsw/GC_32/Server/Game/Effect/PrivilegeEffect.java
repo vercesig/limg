b5afff56843b2f7a3559d8774b22cede58021227
palua <alessandro@debian.com>
2017-07-01 12:58:40 +0200

    fix bug relative to discard leader action

--- src/main/java/it/polimi/ingsw/GC_32/Server/Game/Effect/PrivilegeEffect.java
+++ src/main/java/it/polimi/ingsw/GC_32/Server/Game/Effect/PrivilegeEffect.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import com.eclipsesource.json.Json;
 import com.eclipsesource.json.JsonArray;
+import com.eclipsesource.json.JsonObject;
 import com.eclipsesource.json.JsonValue;
 
 import it.polimi.ingsw.GC_32.Common.Game.ResourceSet;
@@ -14,11 +15,6 @@ import it.polimi.ingsw.GC_32.Server.Game.Player;
 import it.polimi.ingsw.GC_32.Server.Game.Board.Board;
 
 public class PrivilegeEffect {
-	private static ResourceSet[] values = { new ResourceSet("{\"WOOD\": 1, \"STONE\": 1}"),
-											new ResourceSet("{\"SERVANTS\": 2}"),
-											new ResourceSet("{\"COINS\": 2}"),
-											new ResourceSet("{\"MILITARY_POINTS\": 2}"),
-											new ResourceSet("{\"FAITH_POINTS\": 1}")};
 
 	static EffectBuilder buildPrivilege = (JsonValue payload) -> {
 			int number = payload.asObject().get("NUMBER").asInt();
@@ -48,8 +44,8 @@ public class PrivilegeEffect {
 						cm.setContextAck(false, p);
 					} else {
 						for(JsonValue val: response){
-							if(val.isNumber())
-								p.getResources().addResource(values[val.asInt()]);
+							if(val.isNumber()) // to check if the client has type 'n' (only for privilege with cost)
+								p.getResources().addResource(new ResourceSet(Json.parse(val.asString()).asObject()));
 							else
 								isValid = false; // cost privilege effect has been cancelled
 						}
@@ -69,12 +65,12 @@ public class PrivilegeEffect {
 	}
 		
 	private static boolean isSet(JsonArray array){
-		ArrayList<Integer> nums = new ArrayList<>();
+		ArrayList<JsonObject> nums = new ArrayList<>();
 		for(JsonValue val: array){
-			if(nums.contains(val.asInt())){
+			if(nums.contains(Json.parse(val.asString()).asObject())){
 				return false;
 			}
-			nums.add(val.asInt());
+			nums.add(Json.parse(val.asString()).asObject());
 		}
 		return true;
 	}
