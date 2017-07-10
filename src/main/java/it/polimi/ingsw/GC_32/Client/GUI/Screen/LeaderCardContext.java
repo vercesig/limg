7ebb0fdbfef4561a1a4f56e4f439ff607c2252a1
VaporUser <enryruggi@hotmail.it>
2017-07-10 10:10:23 +0200

    others

---
+++ src/main/java/it/polimi/ingsw/GC_32/Client/GUI/Screen/LeaderCardContext.java
@@ -0,0 +1,74 @@
+package it.polimi.ingsw.GC_32.Client.GUI.Screen;
+
+import java.util.ArrayList;
+
+import javafx.scene.AmbientLight;
+import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.PointLight;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class LeaderCardContext {
+	
+	public LeaderCardContext(){};
+	
+	public static void showLeaderCard(ArrayList <LeaderCardGUI> leaderList){ 
+		
+	       	Group family = new Group(); 
+	       	leaderList.forEach(member-> family.getChildren().add(member));
+	        
+	       	AmbientLight light = new AmbientLight(Color.WHITE);
+	        light.setTranslateX(230);
+	        light.setTranslateY(130);
+	        light.setTranslateZ(-180);
+
+	        family.getChildren().add(light);
+	        family.setTranslateX(140);
+	        family.setTranslateY(130);
+	        family.setTranslateZ(-150);
+	       	
+	       
+			for(int i=0; i<leaderList.size(); i++){
+				VBox vbox = new VBox();
+				Label label = new Label(leaderList.get(i).getName());
+//set style				label.set();
+				vbox.getChildren().add(leaderList.get(i));
+				shift(vbox, 1, i*100);
+				shift(label, 0.5, i);
+				family.getChildren().addAll(vbox, label);
+			}
+
+	        Scene window = new Scene(family, 700, 300);
+	        window.setFill(Color.GREY);
+	        window.setCamera(new PerspectiveCamera());
+	        
+	        Stage stage = new Stage();
+	        stage.setTitle("Leader Card view");
+	        stage.setScene(window);
+	        stage.setResizable(false);
+	        stage.show();
+	
+		}
+		
+		private static void shift(VBox vbox, double scale, int factor){
+			vbox.setScaleX(scale);
+			vbox.setScaleY(scale);
+
+			vbox.setTranslateY(-80);
+			vbox.setTranslateX(factor);
+			vbox.setTranslateZ(-40);
+
+		}
+		
+		private static void shift(Label label, double scale, int factor){
+			label.setScaleX(scale);
+			label.setScaleY(scale);
+			label.setTranslateY(100);
+			label.setTranslateX(factor*100+30);
+			label.setTranslateZ(-20);
+		}
+}
---
+++ src/main/java/it/polimi/ingsw/GC_32/Client/GUI/Screen/UserGUI.java
@@ -0,0 +1,81 @@
+package it.polimi.ingsw.GC_32.Client.GUI.Screen;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import it.polimi.ingsw.GC_32.Client.Game.ClientPlayer;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+
+public class UserGUI {
+
+	private static String path = "images/avatars/";
+	private Image image;
+	private ClientPlayer player;
+	private String color;
+
+	//default
+	public UserGUI(ClientPlayer player){
+		this.player = player;
+		this.color = "#FF0000";
+		this.image = new Image(path + "stonemason.png");
+	}
+	
+	//opponent user
+	public UserGUI(ClientPlayer player, int index){
+		this.player = player;
+		switch(index){
+		default: 
+			this.color = "#FF0000"; //red
+			break;
+		case 1:
+			this.color = "#0000FF"; //blue
+			break;
+		case 2:
+			this.color = "#008000"; // green
+			break;
+		case 3:
+			this.color = " #FFFF00"; // yellow
+			break;
+		}
+		Random rand = new Random();
+		int i = rand.nextInt(getAvatarList().size());
+		this.image = new Image(path + getAvatarList().get(i));
+	}
+	
+	public static ArrayList<String> getAvatarList(){
+		ArrayList<String> path = new ArrayList<String>();
+		path.add("abess.png");
+		path.add("ambassador.png");
+		path.add("captain.png");
+		path.add("farmer.png");
+		path.add("governor.png");
+		path.add("paramour.png");
+		path.add("stonemason.png");
+		return path;
+	}
+
+	public static Image getRandom(){
+		Random rand = new Random();
+		int i = rand.nextInt(getAvatarList().size());
+		return new Image(path + getAvatarList().get(i));
+	}
+	
+	public void setUserGUI(String color, Image image){
+		this.color = color;
+		this.image = image;
+	}
+	
+	public ClientPlayer getClientPlayer(){
+		return this.player;
+	}
+	
+	public Image getUserImage(){
+		return this.image;
+	}
+
+	public String getUserColor(){
+		return this.color;
+	}
+}
